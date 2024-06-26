3e23825b1ed58d346d097dd17d967654ed0d9707
ssoyeeon <ssuu2039@gmail.com>
2024-06-26 16:28:01 +0900

    [feat] 새 프로젝트 생성

---
+++ Source/SimSImpul3/SimSImpul3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SimSImpul3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimSImpul3, "SimSImpul3" );
